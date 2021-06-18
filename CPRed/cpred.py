6f81be47feb7887847b3f690099283e5bc2d0226
Daniel Russell <jokr162003@gmail.com>
2021-06-18 03:46:29 -0400

    Stopping point, Prize displaying choices

--- CPRed/cpred.py
+++ CPRed/cpred.py
@@ -677,7 +677,7 @@ class CPRed(commands.Cog):
 
 	@commands.command()
 	async def homebrew(self, ctx, task: str):
-		"""Homebrew rule suggestion and voting system"""
+		"""WIP Homebrew rule suggestion and voting system"""
 		await ctx.send(box("This feature is not yet implemented"))
 
 	@commands.command()
@@ -749,7 +749,7 @@ class CPRed(commands.Cog):
 
 	@commands.command()
 	async def lottery(self, ctx):
-		"""Lottery system *wip"""
+		"""WIP Lottery system"""
 		await ctx.send(box("This feature is not implemented yet"))
 
 	@commands.command()
@@ -764,9 +764,8 @@ class CPRed(commands.Cog):
 		"""Randomly select 3 raffle prizes"""
 		options = await self.config.prizes()
 		selection = random.sample(options, k=3)
-		#sSelection = str(selection)
 		sSelection = '\n'.join(map(str, selection))
-		await ctx.send("Congratulations {}! Please choose one prize from the following:\n".format(user) + sSelection)
+		await ctx.send("Congratulations {}! Please choose one prize from the following:\n\n".format(user) + sSelection)
 
 	@commands.command()
 	async def setfixerlevel(self, ctx, level: int):
